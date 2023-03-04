ee91daea70ee6be431764c8d8f9ad87cdd11ea61
avrunito <125027070+avrunito@users.noreply.github.com>
2023-03-04 17:42:59 +0100

    added printing to file

--- aviation_dataset.py
+++ aviation_dataset.py
@@ -298,13 +298,13 @@ def cross_validation(clf,param_grid,ncv,X_train,y_train,X_test,y_test,name_outpu
 
     #plotting learning curve
     train_sizes = [0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-    #plot_learningcurve(clf,X_train,y_train,train_sizes,ncv,dir_output)
+    plot_learningcurve(clf,X_train,y_train,train_sizes,ncv,dir_output)
     
     resultfile=open('svm/result.txt','w')
     
     resultfile.write('\n SVM best parameters: \n')
-    
-    resultfile.write(search.best_params_)
+    for item in search.best_params_:
+        resultfile.write(search.best_params_[item])
     
     resultfile.close()
     
